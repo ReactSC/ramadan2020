e52819fe81a687d5fb62f1bfa9d4fc7d3f4e1abd
Sofiqul Islam <schoolwab@gmail.com>
2020-04-19 10:37:31 +0600

    Updates

---
+++ precache-manifest.1609f1ac440def9d1a88dead2d18c566.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "922541342bccd297394162c4c4887435",
+    "url": "/ramadan2020/index.html"
+  },
+  {
+    "revision": "ef344c719d8d03544911",
+    "url": "/ramadan2020/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5cb28bf16819ec303871",
+    "url": "/ramadan2020/static/js/2.2cab4323.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ramadan2020/static/js/2.2cab4323.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef344c719d8d03544911",
+    "url": "/ramadan2020/static/js/main.8d0f7f1d.chunk.js"
+  },
+  {
+    "revision": "f146ce1a883b64e66206",
+    "url": "/ramadan2020/static/js/runtime-main.f78b3bf8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ramadan2020/static/media/logo.5d5d9eef.svg"
+  }
+]);
